58a57bd1de08f92711c6a73c0337ba91662e1b48
obunga99 <woofmeow552@gmail.com>
2025-08-04 04:01:07 +0000

    imma take a break for now this is hard

--- main.lua
+++ main.lua
@@ -1,34 +1,47 @@
 function love.load()
 require("cursor")
-require("targets")
+
 
 lg = love.graphics
 targets_holder = {}
-	rect = {}
-	rect.x = 100
-	rect.y = 100
-	rect.w = 20
-	rect.h = 20
-
-
-
+timer = 0
+ammount = 0
+j = 1
+k = 1
 end
 
 function love.update(dt)
+
 	stabilize_cursor()
 	love.mouse.setVisible(false)
-	
+
+	timer = timer + 0.01
+	if timer <= 0.06 and ammount <= 5 then
+		table.insert(targets_holder, create_rectangles())
+		ammount = ammount + 1
+	end
+
 end
 
 function love.draw()
-	lg.setColor(1, 0, 0, 1)
-	lg.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
+	for j, k in ipairs(targets_holder) do
+		lg.rectangle("fill", k.x, k.y, k.w, k.h)
+	end
+
 	init_cursor()
 end
 	
 function love.mousepressed()
-	if love.mouse.isDown("1") and mouse_x >= rect.x and mouse_x <= rect.x + rect.w and mouse_y >= rect.y and mouse_y <= rect.y + rect.h then
-                rect.x = math.random(50, 300)
-                rect.y = math.random(50, 60)
-        end
+	if love.mouse.isDown("1") and mouse_x >= k.x and mouse_x <= k.x + k.w and mouse_y >= k.y and mouse_y <= k.y + k.h then
+		k.x = math.random(60, 300)
+	end
+end
+
+function create_rectangles()
+	rect = {}
+	rect.x = 100
+	rect.y = 100
+	rect.w = 34
+	rect.h = 34
+	return rect
 end
