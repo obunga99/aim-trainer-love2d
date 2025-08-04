027d821b75d47f1be059a7fbd28388fec545014b
obunga99 <woofmeow552@gmail.com>
2025-08-04 10:32:54 +0000

    the five targets finally works :)

--- main.lua
+++ main.lua
@@ -1,13 +1,8 @@
 function love.load()
-require("cursor")
-
-
-lg = love.graphics
-targets_holder = {}
-timer = 0
-ammount = 0
-j = 1
-k = 1
+	require("cursor")
+	require("targets")
+	lg = love.graphics
+	create_target()
 end
 
 function love.update(dt)
@@ -15,33 +10,14 @@ function love.update(dt)
 	stabilize_cursor()
 	love.mouse.setVisible(false)
 
-	timer = timer + 0.01
-	if timer <= 0.06 and ammount <= 5 then
-		table.insert(targets_holder, create_rectangles())
-		ammount = ammount + 1
-	end
-
 end
 
 function love.draw()
-	for j, k in ipairs(targets_holder) do
-		lg.rectangle("fill", k.x, k.y, k.w, k.h)
-	end
-
+	draw_target()
 	init_cursor()
 end
 	
 function love.mousepressed()
-	if love.mouse.isDown("1") and mouse_x >= k.x and mouse_x <= k.x + k.w and mouse_y >= k.y and mouse_y <= k.y + k.h then
-		k.x = math.random(60, 300)
-	end
+	hitscan()
 end
 
-function create_rectangles()
-	rect = {}
-	rect.x = 100
-	rect.y = 100
-	rect.w = 34
-	rect.h = 34
-	return rect
-end
--- targets.lua
+++ targets.lua
@@ -1,11 +1,32 @@
-function create_targets()
-	target = {}
+target = {}
 
-	target.x = math.random(50, 400)
-	target.y = math.random(50, 400)
-	target.w = 10
-	target.h = 10
-	return target
-
+function create_target()
+	for i = 1, 5 do
+		target[i] = {
+			x = math.random(60, lg.getWidth() - 60),
+			y = math.random((lg.getHeight()/2) - 10, (lg.getHeight()/2) + 10),
+			w = 34,
+			h = 34
+		
+		}
+	end
 end
 
+function draw_target()
+	lg.setColor(1, 0, 0, 1)
+	for i, target in ipairs(target) do
+		lg.rectangle("fill", target.x, target.y, target.w, target.h)
+	end
+end
+
+function hitscan()
+	for i, target in ipairs(target) do
+
+		if love.mouse.isDown("1") and mouse_x >= target.x and mouse_x <= target.x + target.w and mouse_y >= target.y and mouse_y <= target.y + target.h then
+
+			target.x = math.random(60, lg.getWidth() - 60)
+			target.y = math.random((lg.getHeight()/2) - 10, (lg.getHeight()/2) + 10)
+			break
+		end
+	end
+end
