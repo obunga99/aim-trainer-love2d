01aaee814895a8bb4c20431b80166d0ac105173b
obunga99 <woofmeow552@gmail.com>
2025-08-04 16:21:31 +0000

    added comments for better readability for myself

--- main.lua
+++ main.lua
@@ -1,23 +1,23 @@
-function love.load()
+function love.load()	--declaration/initialization of variables/files
 	require("cursor")
 	require("targets")
 	lg = love.graphics
-	create_target()
+	create_target() --method from targets.lua for instantiating 5 targets
 end
 
 function love.update(dt)
 
-	stabilize_cursor()
-	love.mouse.setVisible(false)
+	stabilize_cursor() --I dunno 
+	love.mouse.setVisible(false) -- makes the mouse invisible
 
 end
 
 function love.draw()
-	draw_target()
-	init_cursor()
+	draw_target() --draws the target to the screen
+	init_cursor() --draws the cursor
 end
 	
 function love.mousepressed()
-	hitscan()
+	hitscan() --method for detecting when the mouse cursor is clicking the target or in other terms shooting cuz you know this is a aim trainer
 end
 
--- targets.lua
+++ targets.lua
@@ -1,10 +1,10 @@
-target = {}
+target = {} --declare a table for the targets
 
-function create_target()
+function create_target() --a for loop to add a target object from the target table inside another table
 	for i = 1, 5 do
 		target[i] = {
-			x = math.random(60, lg.getWidth() - 60),
-			y = math.random((lg.getHeight()/2) - 10, (lg.getHeight()/2) + 10),
+			x = math.random(60, lg.getWidth() - 60),--this make it go to the middle theoretically
+			y = math.random((lg.getHeight()/2) - 10, (lg.getHeight()/2) + 10),--well this one is the actual one that goes in the middle
 			w = 34,
 			h = 34
 		
@@ -12,21 +12,21 @@ function create_target()
 	end
 end
 
-function draw_target()
-	lg.setColor(1, 0, 0, 1)
-	for i, target in ipairs(target) do
+function draw_target() --draws the target on the screen
+	lg.setColor(1, 0, 0, 1) --sets the color to red
+	for i, target in ipairs(target) do --draws each of the n ammount of targets
 		lg.rectangle("fill", target.x, target.y, target.w, target.h)
 	end
 end
 
-function hitscan()
-	for i, target in ipairs(target) do
+function hitscan() --method for scanning whenever the mouse clicks one of the targets 
+	for i, target in ipairs(target) do --does the whole thing inside a forloop to make sure every target gets involved or some shit
 
-		if love.mouse.isDown("1") and mouse_x >= target.x and mouse_x <= target.x + target.w and mouse_y >= target.y and mouse_y <= target.y + target.h then
+		if love.mouse.isDown("1") and mouse_x >= target.x and mouse_x <= target.x + target.w and mouse_y >= target.y and mouse_y <= target.y + target.h then --checks the collision with the mouse and a target through aab
 
-			target.x = math.random(60, lg.getWidth() - 60)
+			target.x = math.random(60, lg.getWidth() - 60) -- puts it again randomly
 			target.y = math.random((lg.getHeight()/2) - 10, (lg.getHeight()/2) + 10)
-			break
+			break --breaks after it found the target that has been shot or something
 		end
 	end
 end
