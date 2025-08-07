2013ab50ccb7e53b91ce9ab8e8791538e717e747
obunga99 <woofmeow552@gmail.com>
2025-08-07 05:18:20 +0000

    changed the shape of the target from square to circle though the hitbox has not changed it is still square hitbox so you can kinda hit the target outside the edges

--- main.lua
+++ main.lua
@@ -2,12 +2,14 @@ function love.load()	--declaration/initialization of variables/files
 	require("cursor")
 	require("targets")
 	lg = love.graphics
+	distance = 0
 	create_target() --method from targets.lua for instantiating 5 targets
 	
 end
 
 function love.update(dt)
 	stabilize_cursor() --I dunno 
+
 end
 function love.draw()
 	set_background()
@@ -17,6 +19,6 @@ function love.draw()
 end
 	
 function love.mousepressed(x, y, key)
-	hitscan(x, y, key)
+	hitscan(x, y, key, distance)
 end
 
--- targets.lua
+++ targets.lua
@@ -5,8 +5,7 @@ function create_target() --a for loop to add a target object from the target tab
 		target[i] = {
 			x = math.random(30, lg.getWidth() - 30),--this make it go to the middle theoretically
 			y = math.random(10, lg.getHeight() - 10),--well this one is the actual one that goes in the middle
-			cx = x,
-			cy = y,
+			r = 34,
 			w = 34,
 			h = 34
 		
@@ -24,15 +23,16 @@ function draw_target() --draws the target on the screen
 	lg.setColor(0, 0, 0, 1) --sets the color to red
 
 for i, target in ipairs(target) do --draws each of the n ammount of targets
-		lg.rectangle("fill", target.x, target.y, target.w, target.h)
+		lg.circle("fill", target.x, target.y, target.r)
 	end
 end
 
-function hitscan(x, y, key) --method for scanning whenever the mouse clicks one of the targets 
+function hitscan(x, y, key, distance) --method for scanning whenever the mouse clicks one of the targets 
+		for i, target in ipairs(target) do --s the whole thing inside a forloop to make sure every target gets involved or some shit
+	
 
-	for i, target in ipairs(target) do --s the whole thing inside a forloop to make sure every target gets involved or some shit
 
-		if key == 1 and x >= target.x and x <= target.x + target.w and y >= target.y and y <= target.y + target.h then --checks the collision with the mouse and a target through aab
+		if key == 1 and x > target.x - target.w and x < target.x + target.w and y > target.y - target.w and y < target.y + target.h then --checks the collision with the mouse and a target through aab
 			target.x = math.random(30, lg.getWidth() - 30) -- puts it again randomly
 			target.y = math.random(10, lg.getHeight() - 10)
 			break --breaks after it found the target that has been shot or something
