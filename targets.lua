2fb893e4a83ead86458e478ee720e6ce6fb7dd8f
obunga99 <woofmeow552@gmail.com>
2025-08-08 01:14:17 +0000

    fixed the square hitbox for the targets

--- targets.lua
+++ targets.lua
@@ -1,13 +1,11 @@
 target = {} --declare a table for the targets
 
 function create_target() --a for loop to add a target object from the target table inside another table
-	for i = 1, 3  do
+	for i = 1, 5  do
 		target[i] = {
 			x = math.random(30, lg.getWidth() - 30),--this make it go to the middle theoretically
 			y = math.random(10, lg.getHeight() - 10),--well this one is the actual one that goes in the middle
-			r = 34,
-			w = 34,
-			h = 34
+			r = 10
 		
 		}
 	end
@@ -29,19 +27,14 @@ end
 
 function hitscan(x, y, key, distance) --method for scanning whenever the mouse clicks one of the targets 
 		for i, target in ipairs(target) do --s the whole thing inside a forloop to make sure every target gets involved or some shit
-	
-
-
-		if key == 1 and x > target.x - target.w and x < target.x + target.w and y > target.y - target.w and y < target.y + target.h then --checks the collision with the mouse and a target through aab
+			local distance = math.sqrt((x - target.x)^2 + (y - target.y)^2)
+		if key == 1 and distance <= target.r then --checks the collision with the mouse and a target through aab
 			target.x = math.random(math.random(100, 300), lg.getWidth() - math.random(100, 300)) -- puts it again randomly
 			target.y = math.random(math.random(50, 100), lg.getHeight() - math.random(50, 100))
 			score.hits = score.hits + 1
 			break --breaks after it found the target that has been shot or something
-			
 		end
-		if key == 1 and x < target.x - target.w and x > target.x + target.w and y < target.y and y > target.y + target.h then
-			score.miss = score.miss + 1
-		end
+	
 	end
 end
 
