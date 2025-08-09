f5efe88feb53330c1cb08ef2cb48e9ad21e965d9
obunga99 <woofmeow552@gmail.com>
2025-08-09 02:40:14 +0000

    commit

--- targets.lua
+++ targets.lua
@@ -1,7 +1,7 @@
 target = {} --declare a table for the targets
 isHit = false
 function create_target() --a for loop to add a target object from the target table inside another table
-	for i = 1, 4 do
+	for i = 1, 3 do
 		target[i] = {
 			x = math.random(30, lg.getWidth() - 30),--this make it go to the middle theoretically
 			y = math.random(10, lg.getHeight() - 10),--well this one is the actual one that goes in the middle
