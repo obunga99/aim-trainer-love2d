52d4eeff5e38bf813540596f5154428e3aca02c8
obunga99 <woofmeow552@gmail.com>
2025-08-10 21:41:21 +0000

    Added some comments

--- scene/play_scene.lua
+++ scene/play_scene.lua
@@ -16,31 +16,30 @@ function play:load()	--declaration/initialization of variables/files
 end
 
 function play:update(dt)
-	stabilize_cursor() --I dunno 
-	local isTrue = update_score(dt)
+	stabilize_cursor() --Something like init cursor for the true 2d mode
+	local isTrue = update_score(dt)--determines wether to make isTrue a true
 	if isTrue == true then
 		isTrue = false
-		self.setScene("menu")
+		self.setScene("menu")--switches to menu_scene if true
 
 	end
-	camera:lookAt(mouse_x, mouse_y)
+	camera:lookAt(mouse_x, mouse_y)--lock on to the mouse for the fps mode
 
 end
 function play:draw()
 	camera:attach()
-	lg.draw(crosshair, mouse_x, mouse_y)
 	set_background()
 	
 	draw_target() --draws the target to the screen
-	love.mouse.setRelativeMode(true)
+	love.mouse.setRelativeMode(true) -- for the fake fps mode
 	draw_cursor(crosshair)
-	draw_score()
+	draw_score()--draws the timers and stuff
 
 	camera:detach()
 end
 	
 function play:mousepressed(x, y, key)
-	hitscan(x, y, key, distance)
+	hitscan(x, y, key, distance)--the function for shooting the target
 end
 
 
